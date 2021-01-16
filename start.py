31b7ecd42e236f4fa6a066541930d40b39e3a1ee
R34prZ <72569811+R34prZ@users.noreply.github.com>
2021-01-15 21:41:25 -0300

    aaall that bullshit is deleted, just the essencial

--- start.py
+++ start.py
@@ -1,11 +1,6 @@
 import game
 
 def start():
-    global name 
-    name = input("Enter your name: ")
-    print()
-    print("-" * 50)
-    print()
-    print("Hint: You can type MENU to go back to the...ãhhh...menu? ")
-    print()
+    global name
+    name = str(input("Enter your name: "))
     game.game()
