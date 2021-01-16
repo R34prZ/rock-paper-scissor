968e0483b6a10e737c92c10d130711307aacedcd
R34prZ <72569811+R34prZ@users.noreply.github.com>
2021-01-15 21:39:13 -0300

    major change to score system, now it works

--- game.py
+++ game.py
@@ -1,31 +1,26 @@
 # importing the necessary modules
 
 import start
-import random
+from random import choice
 import menu
-import time
+import score
+from time import sleep
 
 # making the possible moves list
 
 t = ["Rock", "Paper", "Scissor"]
 
 # defining the function were all game mechanics are
-
+    
 def game():
-
-    # player movement, computer movement and score
-
-    player_move = str(input("What's your move? "))
-    pc_move = random.choice(t)
-    global score
-    score = 0
-
     # mainloop
 
     run = True
     while run:
 
-        # this piece is my solution to confirm "movement" choice/ quit to the menu
+        player_move = str(input("What's your move? "))
+        pc_move = choice(t)
+        # this piece is my solution to confirm "movement" choice/quit to the menu
 
         print()
         print("_" * 50)
@@ -35,7 +30,7 @@ def game():
         print()
 
         if pause == "N":
-            time.sleep(0.5)
+            sleep(0.5)
             print("Ok leaving to the menu...",)
             menu.menu()
             break
@@ -45,47 +40,38 @@ def game():
         # what to do if the player choice is the same as the computer one (tie)
 
         if player_move.lower() == pc_move.lower():
-            time.sleep(1)
+            sleep(1)
             print("Tie!")
-            game()
-    
+
         # if computer choose Rock
 
-        if pc_move == "Rock":
-            time.sleep(1)
+        elif pc_move == "Rock":
+            sleep(1)
             if player_move.lower() == "paper":
                 print(f"{start.name} wins! No machine can beat him!")
-                score += 1
+                score.player_score += 1
             if player_move.lower() == "scissor":
                 print(f"Computer Wins! {start.name} loses ;(")
-            print(f"score is {score}")
-            game()
+            print(f"score is {score.player_score}")
 
         # if computer choose Paper
 
-        if pc_move == "Paper":
-            time.sleep(1)
+        elif pc_move == "Paper":
+            sleep(1)
             if player_move.lower() == "scissor":
                 print(f"{start.name} wins! No machine can beat him!")
-                score += 1
+                score.player_score += 1
             if player_move.lower() == "rock":
-               print(f"Computer Wins! {start.name} loses")
-            print(f"score is {score}")
-            game()
+                print(f"Computer Wins! {start.name} loses")
+            print(f"score is {score.player_score}")
 
         # if computer choose Scissor
 
-        if pc_move == "Scissor":
-            time.sleep(1)
+        elif pc_move == "Scissor":
+            sleep(1)
             if player_move.lower() == "rock":
                 print(f"{start.name} wins! No machine can beat him!")
-                score += 1
+                score.player_score += 1
             if player_move.lower() == "paper":
                 print(f"{start.name} was cutted! PC wins...")
-            print(f"score is {score}")
-            game()
-
-# as you can see after the possible events functions there is a call to the "game()" function, this is made so you can choose
-# Rock, Paper or Scissor every turn
-# probably i could make that inside the loop without calling the funtion (i think in the actual state it is "recursive") but 
-# i was to lazy to trie it
+            print(f"score is {score.player_score}")
