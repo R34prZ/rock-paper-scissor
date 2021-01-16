c81cbc3250f7419030a37a6cf9a06e2239719b78
R34prZ <72569811+R34prZ@users.noreply.github.com>
2021-01-16 15:28:02 -0300

    u can now view all the hints with the HINT command

--- help.py
+++ help.py
@@ -1,13 +1,21 @@
 import menu
+from time import sleep
 
 # this is the help() function, it contains just the basic game rules for now
 
 def help():
     print("-" * 50)
     print("Rules:\n1. Rock crushes Scissor, \n2. Scissor cuts Paper, \n3. Paper covers Rock.")
-    print("You can go back to menu typing MENU")
+    print("You can go back to menu typing MENU, or view all the hints with HINTS")
     print("-" * 50)
 
     go_back = str(input("Go back? "))
+
+    if go_back.lower() == "hints":
+        for hint in menu.HINTS:
+            print(hint)
+            sleep(1)
+        help()
+            
     if go_back.lower() == "menu":
         menu.menu()
